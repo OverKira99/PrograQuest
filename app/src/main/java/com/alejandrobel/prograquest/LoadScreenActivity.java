290f908477ce50ec1c78e0fbafeec7093133cab8
Alejandro <lbeltran598@unab.edu.co>
2024-11-11 15:59:53 -0500

    pantalla de carga inicial

--- app/src/main/java/com/alejandrobel/prograquest/LoadScreenActivity.java
+++ app/src/main/java/com/alejandrobel/prograquest/LoadScreenActivity.java
@@ -1,8 +1,12 @@
 package com.alejandrobel.prograquest;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,13 +21,28 @@ public class LoadScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_screen);
 
-        // Simula un retraso al iniciar session
-        new Handler().postDelayed(() -> {
+        ImageView logoImageView = findViewById(R.id.logoImageView);
+        TextView loadingTextView = findViewById(R.id.loadingTextView);
 
-            // pasa a la siguiente activity
-            startActivity(new Intent(LoadScreenActivity.this, LoginActivity.class));
-            finish(); // Cerrar la SplashActivity
-        }, 200);
+        // Animación de rotación para el logo
+        ObjectAnimator rotation = ObjectAnimator.ofFloat(logoImageView, "rotation", 0f, 360f);
+        rotation.setDuration(2000);
+        rotation.setRepeatCount(ValueAnimator.INFINITE);
+        rotation.start();
+
+        // Animación de parpadeo para el texto de carga
+        ObjectAnimator fadeInOut = ObjectAnimator.ofFloat(loadingTextView, "alpha", 0f, 1f);
+        fadeInOut.setDuration(1000);
+        fadeInOut.setRepeatCount(ValueAnimator.INFINITE);
+        fadeInOut.setRepeatMode(ValueAnimator.REVERSE);
+        fadeInOut.start();
+
+        // Redirigir a la siguiente actividad después de 3 segundos
+        new android.os.Handler().postDelayed(() -> {
+            Intent intent = new Intent(LoadScreenActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish(); // Cierra la actividad de splash para que no vuelva al retroceder.
+        }, 3000); // 3 segundos de espera
     }
 
 }
